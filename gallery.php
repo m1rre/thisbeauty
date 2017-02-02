5118d5408357da44b5d2ea48cc666862f461e470
hajarrashidi <hajarrashidi@gmail.com>
2017-02-02 15:57:17 +0100

    Added Gallery page with 3 sample images

--- gallery.php
+++ gallery.php
@@ -1,4 +1,71 @@
 <?php require 'template/header.php' ?>
+<div class="row">
+
+    <div class="col-lg-12">
+        <h1 class="page-header">Gallery</h1>
+    </div>
+
+    <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="assets/img/1.jpg" alt="">
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="assets/img/2.jpg" alt="">
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="assets/img/3.jpg" alt="">
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+        </a>
+    </div>
+</div>
 
 
 <?php require 'template/footer.php' ?>
