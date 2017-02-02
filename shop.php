7f8e7483fb06a3a542dd65a6f6dce363e8f7e9cf
William <william_K_nilsson@hotmail.com>
2017-02-02 16:16:33 +0100

    bilder, beskrivningar, priser mm i shop

--- shop.php
+++ shop.php
@@ -14,17 +14,18 @@
     <div id="products" class="row list-group">
         <div class="item  col-xs-4 col-lg-4">
             <div class="thumbnail">
-                <img class="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
+                <img class="group list-group-image" src="assets/img/1.jpg" alt="" />
                 <div class="caption">
                     <h4 class="group inner list-group-item-heading">
-                        Product title</h4>
+                        PHB All in One Natural Mascara - Black</h4>
                     <p class="group inner list-group-item-text">
-                        Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-                        sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                        Har du korta eller glesa ögonfransar? Eller överplockat dina ögonbryn?
+                         Ingen fara, med Organyc Ögonfrans- och bryn serum stimuleras din egna hårväxt vilket ger tätare och längre fransar inom bara 60 dagar.
+                         Serumet är allergitestat och passar för känsliga ögon. Helt fritt från parabener och konserveringsmedel. </p>
                     <div class="row">
                         <div class="col-xs-12 col-md-6">
                             <p class="lead">
-                                $21.000</p>
+                                159 kr</p>
                         </div>
                         <div class="col-xs-12 col-md-6">
                             <a class="btn btn-success" href="#">Add to cart</a>
@@ -35,17 +36,17 @@
         </div>
         <div class="item  col-xs-4 col-lg-4">
             <div class="thumbnail">
-                <img class="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
+                <img class="group list-group-image" src="assets/img/2.jpg" alt="" />
                 <div class="caption">
                     <h4 class="group inner list-group-item-heading">
-                        Product title</h4>
+                        Beauty by Earth Makeup Remover</h4>
                     <p class="group inner list-group-item-text">
-                        Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-                        sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                        Oljefri makeup remover som skonsamt tar bort smink kring dina ögon utan att skada dina ögonfransförlängningar.
+                         Allergitestad och rekommenderad till dig med känslig hy. Fri från animaliska produkter och parabener. </p>
                     <div class="row">
                         <div class="col-xs-12 col-md-6">
                             <p class="lead">
-                                $21.000</p>
+                                139 kr</p>
                         </div>
                         <div class="col-xs-12 col-md-6">
                             <a class="btn btn-success" href="#">Add to cart</a>
@@ -56,17 +57,18 @@
         </div>
         <div class="item  col-xs-4 col-lg-4">
             <div class="thumbnail">
-                <img class="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
+                <img class="group list-group-image" src="assets/img/3.jpg" alt="" />
                 <div class="caption">
                     <h4 class="group inner list-group-item-heading">
-                        Product title</h4>
+                        Eyelash Extensions Blink Black Diamond Coating Sealant</h4>
                     <p class="group inner list-group-item-text">
-                        Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-                        sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                        Det bästa alternativet på marknaden när det kommer till eftervård av dina ögonfransförlängningar.
+                        Genom att försegla fransarna kan njuta av långa fylliga fransar ännu längre. Fri från parabener och allergitestad.
+                        Passar dig med känsliga ögon. </p>
                     <div class="row">
                         <div class="col-xs-12 col-md-6">
                             <p class="lead">
-                                $21.000</p>
+                                129 kr</p>
                         </div>
                         <div class="col-xs-12 col-md-6">
                             <a class="btn btn-success" href="#">Add to cart</a>
