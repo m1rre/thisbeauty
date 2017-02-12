ef40940236939dd302e3f8be0b069abb003616cd
Ditte Gunterberg <ditte-g@users.noreply.github.com>
2017-02-12 17:00:04 +0100

    Display orders and bookings API (my page)

---
+++ API/my_page_order.php
@@ -0,0 +1,19 @@
+<?php
+
+include("auth.php"); //include auth.php file on all secure pages
+require('db.php');
+
+$email = $_SESSION['email'];
+
+//Visar användarens köp
+$stm_select = $pdo->prepare('SELECT * FROM `order` WHERE email = :email');
+$stm_select->execute(['email' => $email]);
+$resultat = array();
+
+foreach($stm_select as $row) {
+    $resultat[] = $row;
+
+}
+
+//JSON_UNESCAPED_UNICODE används för att kunna skriva ut åäö.
+echo json_encode($resultat, JSON_UNESCAPED_UNICODE);
---
+++ API/my_page_time.php
@@ -0,0 +1,21 @@
+<?php
+
+include("auth.php"); //include auth.php file on all secure pages
+require('db.php');
+
+$email = $_SESSION['email'];
+
+//Visar användarens bokade tider
+$stm_select = $pdo->prepare('SELECT * FROM `timebooking` WHERE email = :email');
+$stm_select->execute(['email' => $email]);
+$resultat = array();
+
+foreach($stm_select as $row) {
+    $resultat[] = $row;
+
+}
+
+//JSON_UNESCAPED_UNICODE används för att kunna skriva ut åäö.
+echo json_encode($resultat, JSON_UNESCAPED_UNICODE);
+?>
+
