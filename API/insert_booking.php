f724b4751e007b918c564b36a52790d6fd48a29c
kaisasvensson <kaisa.svensson@hotmail.com>
2017-02-14 09:30:35 +0100

    Update insert_booking.php

--- API/insert_booking.php
+++ API/insert_booking.php
@@ -3,47 +3,43 @@
 
 require("db.php");
 
-//-------------------> KOLLA SÅ DATUM/TID INTE DUBBLERAS (tog bort email) lade till variablar till alla namn.
+//-------------------> KOLLA SÅ DATUM/TID INTE DUBBLERAS
 
-	if( isset($_GET['time']) ) {
+	if( isset($_POST['time']) ) {
 
    		$date = $_POST['date'];
 		$time = $_POST['time'];
 		$type = $_POST['type'];
 		$newtime = $date . " " . $time;
 
-//lägger till variablen newtime så den inkluderar både date och time, här kollar vi om det redan finns bokad tid.
+//lägger till variablen newtime så den inkluderar både date och time
+		
 		$sql = "SELECT * FROM `timebooking` WHERE `bookingtime` = '{$newtime}'";
 		$stm_count = $pdo->prepare($sql);
 		$stm_count->execute();
 
-// $cnt (count) räknar from 0  alltså stm_count räknar alla rader from 0
 	$cnt = 0;	
 	foreach($stm_count as $row) {
 		$cnt++;		
 }
 
-//-------------------> LÄGG TILL NYA VÄRDEN I DATABASEN tagit bort att uppdatera. obs använde endast cnt för att räkna antal bokningar
+//-------------------> LÄGG TILL NYA VÄRDEN I DATABASEN
 
   	if($cnt >= 1 ) {
-		/*  echo "tiden är redan bokad";*/
+		// echo "tiden är redan bokad";
 		echo json_encode(false);
 
-	} else /*{
-		
-			try*/ {
+	} else {
 				$sql = "INSERT INTO `timebooking` (`bookingtime`, `bookingtype`) VALUES ('{$newtime}', '{$type}')";
 				$stm_insert = $pdo->prepare($sql);
 				$stm_insert->execute();
-				/*echo "<p>Lade till bokning</p>";*/
+				//echo "<p>Lade till bokning</p>";
 				echo json_encode(true);
 
 
-	 } /*catch (PDOExeption $e) {
-			echo $e;
-		}*/
-	}
-/*}	*/
+	 } 
+}
+
 
 ?>
 
