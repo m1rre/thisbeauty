6dc02ff18d8916a3582608b281a254626f18fa6b
Ditte Gunterberg <ditte-g@users.noreply.github.com>
2017-02-14 15:26:49 +0100

    API update address (my page)
    Används för att användaren ska kunna uppdatera sin adress på mina sidor (adress, postnummer, postort)

---
+++ API/my_page_update_address.php
@@ -0,0 +1,14 @@
+<?php
+
+require('db.php');
+include("auth.php");
+$email = $_SESSION['email'];
+
+if( isset($_POST['faddress']) ) {
+    $sql = "UPDATE `users` SET `address` = :vaddress, `zipcode` = :vzipcode, `city` = :vcity WHERE `email` = :vemail";
+    $stm_update = $pdo->prepare($sql);
+    $stm_update->execute(['vaddress' => $_POST['faddress'], 'vzipcode' => $_POST['fzipcode'], 'vcity' => $_POST['fcity'], 'vemail' => $email]);
+    echo json_encode(true);
+    }
+
+?>
