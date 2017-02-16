58cf1642a07b13de248e070006dff416bf46072c
kaisasvensson <kaisa.svensson@hotmail.com>
2017-02-16 11:04:41 +0100

    Update insert_session.php

--- API/insert_session.php
+++ API/insert_session.php
@@ -1,7 +1,8 @@
 <?php
 
 session_start();
-		
+
+//gör en array som innehåller tid/datum och typ av behandling för att se resultatet.
 
 if (empty($_SESSION['timebooking'])) {
 $_SESSION['timebooking'] = array();
