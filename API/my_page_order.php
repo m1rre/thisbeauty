364d4734c0d4ba49d4f311778238110c442b155b
Ditte Gunterberg <ditte-g@users.noreply.github.com>
2017-02-16 09:58:07 +0100

    Update my_page_order.php

--- API/my_page_order.php
+++ API/my_page_order.php
@@ -6,7 +6,7 @@ require('db.php');
 $email = $_SESSION['email'];
 
 //Visar användarens köp
-$stm_select = $pdo->prepare('SELECT * FROM `order` WHERE email = :email');
+$stm_select = $pdo->prepare('SELECT * FROM `order` WHERE email = :email'); //Väljer den e-postadress som är inloggad
 $stm_select->execute(['email' => $email]);
 $resultat = array();
 
@@ -17,3 +17,4 @@ foreach($stm_select as $row) {
 
 //JSON_UNESCAPED_UNICODE används för att kunna skriva ut åäö.
 echo json_encode($resultat, JSON_UNESCAPED_UNICODE);
+?>
