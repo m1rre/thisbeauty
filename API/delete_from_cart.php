8475e4517e8e33cdee8a011fac44b55ed4a80a77
sdamgren <sandra.damgren@hotmail.com>
2017-02-16 09:43:51 +0100

    Update delete_from_cart.php
    Added comment

--- API/delete_from_cart.php
+++ API/delete_from_cart.php
@@ -1,30 +1,13 @@
 <?php
 session_start();
-require 'db.php';
 
 if (isset ($_GET['id'])) {
+require 'db.php';
 
-
-    $stm_select = $pdo->prepare('SELECT * FROM products WHERE id='.$_GET['id']);
-    $stm_select->execute();
-    foreach($stm_select as $row) {
-       
-        if(!empty($_SESSION['cart']))
-            if(is_array($_SESSION['cart']))
-            {
-                array_push($_SESSION['cart'], [ "item"=>$row, "quantity"=>1] );
-            }
-            else
-            {
-                $_SESSION['cart'] = array( [ "item"=>$row, "quantity"=>1] );
-            }
-        else
-            $_SESSION['cart'] = array( [ "item"=>$row, "quantity"=>1] );
-
-        echo json_encode(array_splice($_SESSION['cart'], 0, 1));
+echo json_encode(array_splice($_SESSION['cart'], 0, 1));
     
-    //array_splice($_SESSION['cart'], 0, 1);
+    //array_splice = Remove elements from an array and replace it with new elements
 
-    }
+    
 }
 ?>
